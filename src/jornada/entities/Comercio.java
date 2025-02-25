2b9516a38b28b304872fe7dd7c3fec0e8a0f8582
Aleliks <72809824+Alelix-themage@users.noreply.github.com>
2025-02-25 10:16:39 -0300

    Inclua o comprar vida do comercio

--- src/jornada/entities/Comercio.java
+++ src/jornada/entities/Comercio.java
@@ -2,28 +2,23 @@ package jornada.entities;
 
 
 
-public class Comercio extends Personagem  {
+public class Comercio   {
     // variável que armazena o ganho do comércio
     private float arrecadacao = 0;
     private float valor_habilidade = 5;
     private float cura = 5;
 
-    @Override
-    public void comer() {
-        System.out.println("O comércio não precisa comer.");
-    }
 
-    public void comprarVida(float preco){
+    public float comprarVida(float preco){
         if(preco != valor_habilidade){
             System.out.println("Você não tem dinheiro o suficiente para comprar uma habilidade!");
+            return 0;
         }
         else {
            this.arrecadacao = this.arrecadacao + preco;
            System.out.println("Você acaba de adquirir uma poção de cura!");
-           curar(cura);
 
-           System.out.println("Você acaba de Ser curado!");
-           getVida();
+           return cura;
            
         }
     }
